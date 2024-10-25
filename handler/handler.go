a0a1cdde7233698a01005d09d2066e351cef3658
tangfire <52628216@qq.com>
2024-10-25 09:36:11 +0800

    Upload,GetFileMeta

---
+++ handler/handler.go
@@ -0,0 +1,93 @@
+package handler
+
+import (
+	"encoding/json"
+	"filestore-server/meta"
+	"filestore-server/util"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+// UploadHandler:处理文件上传
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		// 返回html页面
+		data, err := os.ReadFile("./static/view/index.html")
+		if err != nil {
+			io.WriteString(w, "internet server error")
+			return
+		}
+		io.WriteString(w, string(data))
+	} else if r.Method == "POST" {
+		// 接收文件流及存储到本地目录
+		file, head, err := r.FormFile("file")
+		if err != nil {
+			fmt.Printf("Failed to get data,err:%s\n", err.Error())
+			return
+		}
+		defer file.Close()
+
+		fileMeta := meta.FileMeta{
+			FileName: head.Filename,
+			Location: "/tmp/" + head.Filename,
+			UploadAt: time.Now().Format("2006-01-02 15:04:05"),
+		}
+
+		//newFile, err := os.Create("/tmp/" + head.Filename)
+		newFile, err := os.Create(fileMeta.Location)
+		if err != nil {
+			fmt.Printf("Failed to create file,err:%s\n", err.Error())
+			return
+		}
+		defer newFile.Close()
+
+		fileMeta.FileSize, err = io.Copy(newFile, file)
+		if err != nil {
+			fmt.Printf("Failed to save data into file,err:%s\n", err.Error())
+			return
+		}
+
+		//newFile.Seek(0, 0) 的作用是将文件指针移动到文件的开头。这在许多场景中是非常有用的，尤其是在想要重新读取文件的内容时。
+		//
+		//典型应用场景
+		//重新读取文件: 如果你之前已经读取了文件的某些内容，但需要再次从文件的开头开始读取，可以使用 Seek(0, 0) 将指针移动到开头。
+		//
+		//在写入文件之前清空内容: 在创建文件并写入数据之前，将指针移动到开头是常见的操作。
+		//
+		//在流式处理中: 在处理大文件或流式数据时，可能会需要多次读取相同的文件，使用 Seek(0, 0) 可以方便地重置指针。
+		newFile.Seek(0, 0)
+		fileMeta.FileSha1 = util.FileSha1(newFile)
+		meta.UpdateFileMeta(fileMeta)
+
+		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
+
+	}
+}
+
+// UploadSucHandler:上传已完成
+func UploadSucHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Upload finished!")
+
+}
+
+// GetFileMetaHandler:获取文件元信息
+func GetFileMetaHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	// sha1sum 是一个常见的命令行工具，用于计算和验证文件的 SHA-1 哈希值。
+	//SHA-1（Secure Hash Algorithm 1）是一种加密哈希函数，它将任意大小的数据输入转换为固定大小的 160 位（20 字节）哈希值。
+	//sha1sum 工具广泛用于文件完整性验证、数据完整性校验以及数字签名。
+	// example: sha1sum filename.txt
+
+	filehash := r.Form["filehash"][0]
+	fMeta := meta.GetFileMeta(filehash)
+	data, err := json.Marshal(fMeta)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
+}
---
+++ util/util.go
@@ -0,0 +1,75 @@
+package util
+
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"encoding/hex"
+	"hash"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+// 在结构体 Sha1Stream 中，定义了一个名为 _sha1 的字段，类型为 hash.Hash。
+//hash.Hash 是一个接口，定义了方法用于计算哈希值。具体实现该接口的类型（如 SHA-1、SHA-256 等）可以用于处理数据并生成相应的哈希值。
+//通过将 hash.Hash 接口嵌入 Sha1Stream，可以在 Sha1Stream 中使用所有实现了 hash.Hash 接口的方法
+
+type Sha1Stream struct {
+	_sha1 hash.Hash
+}
+
+func (obj *Sha1Stream) Update(data []byte) {
+	if obj._sha1 == nil {
+		obj._sha1 = sha1.New()
+	}
+	obj._sha1.Write(data)
+}
+
+func (obj *Sha1Stream) Sum() string {
+	return hex.EncodeToString(obj._sha1.Sum([]byte("")))
+
+}
+
+func Sha1(data []byte) string {
+	_sha1 := sha1.New()
+	_sha1.Write(data)
+	return hex.EncodeToString(_sha1.Sum(nil))
+}
+
+func FileSha1(file *os.File) string {
+	_sha1 := sha1.New()
+	io.Copy(_sha1, file)
+	return hex.EncodeToString(_sha1.Sum(nil))
+}
+
+func MD5(data []byte) string {
+	_md5 := md5.New()
+	_md5.Write(data)
+	return hex.EncodeToString(_md5.Sum([]byte("")))
+}
+
+func FileMD5(file *os.File) string {
+	_md5 := md5.New()
+	io.Copy(_md5, file)
+	return hex.EncodeToString(_md5.Sum(nil))
+}
+
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func GetFileSize(filename string) int64 {
+	var result int64
+	filepath.Walk(filename, func(path string, f os.FileInfo, err error) error {
+		result = f.Size()
+		return nil
+	})
+	return result
+}
